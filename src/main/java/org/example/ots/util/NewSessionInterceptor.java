de375cd90a07bae0c29afb7071e3107707568051
Harald Fassler <harald.fassler+9974@gmail.com>
2021-06-11 06:06:52 +0200

    switch to logback

--- src/main/java/org/example/ots/util/NewSessionInterceptor.java
+++ src/main/java/org/example/ots/util/NewSessionInterceptor.java
@@ -2,18 +2,19 @@ package org.example.ots.util;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.struts2.ServletActionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NewSessionInterceptor extends AbstractInterceptor {
 
-    private static final Logger LOGGER = LogManager.getLogger(NewSessionInterceptor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(NewSessionInterceptor.class);
 
     @Override
     public String intercept(ActionInvocation invocation) throws Exception {
         try {
             ServletActionContext.getRequest().getSession().invalidate();
+            LOGGER.debug("session invalidated");
         } catch (Exception ex) {
             LOGGER.error("could not invalidate the session", ex);
         }
