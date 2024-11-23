fa407b30618358d9790ec3bbe24a02b897839d63
KaporMirr <188729543+KaporMirr@users.noreply.github.com>
2024-11-23 05:50:54 +0100

    Update kokota

--- src/main/java/org/example/ots/action/IndexAction.java
+++ src/main/java/org/example/ots/action/IndexAction.java
@@ -1,16 +1,1 @@
-package org.example.ots.action;
 
-import com.opensymphony.xwork2.ActionSupport;
-
-public class IndexAction extends ActionSupport {
-
-    @Override
-    public String execute() {
-        return SUCCESS;
-    }
-
-    public String returnSuccess() {
-        return SUCCESS;
-    }
-
-}
